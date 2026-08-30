707b0469907d03c2d5f5950d8b33d570e06936ce
Franciscoestenio-1981 <estenio_gino@yahoo.com.br>
2026-08-30 14:46:37 +0000

    feat: adicionar dados simulados

---
+++ api/src/mocks/user.mock.ts
@@ -0,0 +1,20 @@
+import type {user} from '../types/user.types.ts';
+
+const users: user[] = [{
+    id: 1,
+    name: 'Alex bessa',
+    email: 'alex@email.com',
+    password: 'jd*123',
+},{
+    id: 2,
+    name: 'estenio',
+    email: 'estenio@email.com',
+    password: 'ga*546',
+}, {
+    id: 3,
+    name: 'conrado',
+    email: 'conrado@email.com',
+    password: '233b*99',    
+}];
+
+export default users;
