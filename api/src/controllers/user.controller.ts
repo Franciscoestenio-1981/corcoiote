3d2ff7e911e7fe4ccf94cc62dfaf76cbf9abeb1b
Franciscoestenio-1981 <estenio_gino@yahoo.com.br>
2026-09-06 14:45:22 +0000

    feat: implementar interface de aplicacao para busca de usuario unico

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,8 +1,19 @@
 import type { Request, Response } from 'express';
-import { findAllUsers } from "../services/user.service.ts";
+import { 
+    findAllUsers,
+    findUserById 
+    } from "../services/user.service.ts";
 
 export function getAllUsers(request: Request, response: Response) {
     const users = findAllUsers();
 
     response.status(200).json(users);
+}
+
+export function getUserById(request: Request, response: Response) {
+const id = request.params.id;
+
+const user = findUserById(Number(id));
+
+response.status(200).json(user);
 }
--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -3,3 +3,13 @@ import users from "../mocks/user.mock.ts";
 export function findAllUsers() {
     return users;
 }
+
+export function findUserById(id: number) {
+    const user = users.find(user => user.id === id);
+
+    if (!user)
+        throw new Error(`Usuário de id ${id} não encontrado.`);
+
+    return user;
+
+}
